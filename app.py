77c87cc276f907eee27ff3afe977aeac461ad3bb
Bilal <t-muhammad.bilal@paypay-corp.co.jp>
2025-09-29 17:58:38 +0500

    Last Update hai pakka5.0

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def add_item():
     return jsonify({"message": "Item added successfully"}), 201
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=0)
 
