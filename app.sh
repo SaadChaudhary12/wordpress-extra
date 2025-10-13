c5bbd5fb8c83a019592ae1dd81239ca7221c26ee
Bilal <t-muhammad.bilal@paypay-corp.co.jp>
2025-10-13 15:46:42 +0500

    Add workflow to upload Flask app to S3s

--- app.sh
+++ app.sh
@@ -8,38 +8,56 @@ sudo dnf install -y unzip awscli python3
 sudo dnf install -y mariadb105-server
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
-sudo dnf install -y python3 git
-sudo dnf install -y python3-pip
+sudo yum update -y
+sudo yum install -y python3 pip unzip awscli
 
+LATEST_ZIP=$(aws s3 ls s3://terraform-bucket-test20/ --recursive | sort | tail -n 1 | awk '{print $4}')
+aws s3 cp s3://terraform-bucket-test20/$LATEST_ZIP /home/ec2-user/app.zip
 cd /home/ec2-user
+unzip -o app.zip
+pip3 install flask pymysql boto3
+nohup python3 app_lt.py > /home/ec2-user/app.log 2>&1 &
+
+# sudo yum update -y
+# sudo dnf install nginx -y
+# sudo systemctl start nginx
+# sudo systemctl enable nginx
+# sudo dnf install -y unzip awscli python3
+# sudo dnf install -y mariadb105-server
+# sudo systemctl start mariadb
+# sudo systemctl enable mariadb
+# sudo dnf install -y python3 git
+# sudo dnf install -y python3-pip
+
+# cd /home/ec2-user
 
 
-if [ ! -d "wordpress-extra" ]; then
-    git clone https://github.com/SaadChaudhary12/wordpress-extra.git
-fi
+# if [ ! -d "wordpress-extra" ]; then
+#     git clone https://github.com/SaadChaudhary12/wordpress-extra.git
+# fi
 
-DB_HOST="${db_endpoint}"
-DB_USER="Application"
-DB_PASS="Application"
-DB_NAME="Application"
+# DB_HOST="${db_endpoint}"
+# DB_USER="Application"
+# DB_PASS="Application"
+# DB_NAME="Application"
 
-mysql -h $DB_HOST -u $DB_USER -p$DB_PASS <<EOF
+# mysql -h $DB_HOST -u $DB_USER -p$DB_PASS <<EOF
 
-USE Application;
-CREATE TABLE items (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100),
-  quantity INT
-);
+# USE Application;
+# CREATE TABLE items (
+#   id INT AUTO_INCREMENT PRIMARY KEY,
+#   name VARCHAR(100),
+#   quantity INT
+# );
 
-EOF
+# EOF
 
 
-aws s3 cp s3://terraform-bucket-test20/app_package.zip /home/ec2-user/app_package.zip
-unzip -o /home/ec2-user/app_package.zip -d /home/ec2-user/wordpress-extra/
-cd /home/ec2-user/wordpress-extra
+# aws s3 cp s3://terraform-bucket-test20/app_package.zip /home/ec2-user/app_package.zip
+# unzip -o /home/ec2-user/app_package.zip -d /home/ec2-user/wordpress-extra/
+# cd /home/ec2-user/wordpress-extra
 
-sudo chown -R ec2-user:ec2-user /home/ec2-user/wordpress-extra
+# sudo chown -R ec2-user:ec2-user /home/ec2-user/wordpress-extra
 
-pip3 install -r requirements.txt
-nohup python3 app_lt.py > app2.log 2>&1 &
+# pip3 install -r requirements.txt
+# nohup python3 app_lt.py > app2.log 2>&1 &
