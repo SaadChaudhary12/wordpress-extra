d66b2a96483c224f8ade9aa0b836c164f282feb5
Bilal <t-muhammad.bilal@paypay-corp.co.jp>
2025-10-15 19:43:14 +0500

    Add workflow to upload Flask app to S3s-Last-1336=7

--- app_lt.py
+++ app_lt.py
@@ -1,5 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from flask import Flask, request, jsonify
-from utils import utils
+import utils.utils as utils
 
 app = Flask(__name__)
 
